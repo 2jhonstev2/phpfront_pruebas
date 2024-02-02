e59f871bd2db3a3c166864c2523703e604700de9
Jhon S. Vique <jhonsteven827@gmail.com>
2024-02-02 15:13:11 -0500

    First Commit upload files structure

---
+++ app/Http/Controllers/EditorialsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Editorials;
+use Illuminate\Http\Request;
+
+class EditorialsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Editorials  $editorials
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Editorials $editorials)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Editorials  $editorials
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Editorials $editorials)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Editorials  $editorials
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Editorials $editorials)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Editorials  $editorials
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Editorials $editorials)
+    {
+        //
+    }
+}
